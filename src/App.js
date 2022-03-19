3ceea7fdb226d69af7e66ef9805fca88b1df549e
Pamela Badermann <badermann.pamela@gmail.com>
2022-03-19 20:35:24 -0300

    tema main com mui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material';
 
+import { mainTheme } from './config/theme/main';
 import { Router } from './routes';
 
 function App() {
     return (
         <React.Fragment>
-            <Router></Router>
+            <ThemeProvider theme={mainTheme}>
+                <Router></Router>
+            </ThemeProvider>
         </React.Fragment>
     );
 }
---
+++ src/config/theme/main.jsx
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material';
+
+export const mainTheme = createTheme({
+    palette: {
+        background: {},
+        primary: {
+            main: '#6200ee',
+        },
+    },
+    typography: {
+        h4: {
+            color: '#6200ee',
+        },
+    },
+});
